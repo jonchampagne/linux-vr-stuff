531c4cbf61e42d61e29ef1aa51a425a95e1d2adc
Jon Champagne <jon@jchampagne.ca>
2024-12-27 23:10:16 -0500

    Updates

--- start-vr.sh
+++ start-vr.sh
@@ -1,8 +1,25 @@
 #!/bin/env bash
 
-# Enable VR mode on AMD gpu
-pkexec env WAYLAND_DISPLAY="$XDG_RUNTIME_DIR/$WAYLAND_DISPLAY" XDG_RUNTIME_DIR=/run/user/0 ./enable-vr-mode.sh
+dir=`dirname -- "$( readlink -f -- "$0"; )"`
+
+cd $dir
 
 # Set the default audio device to usb headphones
 pactl set-default-sink `pactl list sinks short|grep KTMicro_KT_USB_Audio|cut -f1`
 
+# Set the default audio device to bigscreen beyond mic
+pactl set-default-source `pactl list sources short|grep Bigscreen_Beyond|cut -f1`
+
+sleep 1
+echo "Starting wlx-overlay-s"
+wlx-overlay-s --openxr --show &
+
+sleep 5
+
+echo "Starting lovr-playspace"
+pushd lovr-playspace
+../lovr.AppImage . &
+popd
+
+# Enable VR mode on AMD gpu
+pkexec env WAYLAND_DISPLAY="$XDG_RUNTIME_DIR/$WAYLAND_DISPLAY" XDG_RUNTIME_DIR=/run/user/0 `realpath ./enable-vr-mode.sh`
